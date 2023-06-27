6d9bc3370ca3ea0364b15f0f268b8bc375258fe6
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-06-27 20:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function CreatPromise{
+	return new Promise((resolve, reject)=>{
+		setTimeOut({
+			resolve("Hello, world!")
+		},1000)
+	})
+}
+CreatPromise().then((data) =>{
+	const element= document.getElementById("output");
+	element.innerText= data;
+})
